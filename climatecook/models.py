28f49c199f7a11a590ad7e8a8053eba1fa073dab
Toni Kuosmanen <toni.t.kuosmanen@gmail.com>
2020-02-13 21:17:22 +0200

    add remaining models

--- climatecook/models.py
+++ climatecook/models.py
@@ -4,7 +4,6 @@ from sqlalchemy import CheckConstraint
 from climatecook import db
 
 
-
 @click.command("init-db")
 @with_appcontext
 def init_db_command():
@@ -12,15 +11,87 @@ def init_db_command():
 
 
 class Recipe(db.Model):
-    __tablename__ = "Recipes"
     id = db.Column(db.Integer, primary_key=True)
     recipe_category_id = db.Column(db.Integer, db.ForeignKey('recipe_category.id'), nullable=True)
     name = db.Column(db.String(64), nullable=False)
+
     recipe_category = db.relationship("RecipeCategory", back_populates="recipes")
+    ratings = db.relationship("Rating", back_populates="recipe")
+    ingredients = db.relationship("Ingredient", back_populates="recipe")
+
     __table_args__ = (CheckConstraint('length(name) >= 1', name='cc_recipe_name'),)
 
 
 class RecipeCategory(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String(64), nullable=False)
+
     recipes = db.relationship("Recipe", back_populates="recipe_category")
+
+
+class Rating(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    rating = db.Column(db.Integer, nullable=False)
+    comment = db.Column(db.String(2048), nullable=False)
+
+    recipe = db.relationship("Recipe", back_populates="ratings")
+
+    __table_args__ = (CheckConstraint('rating >= 1 AND rating <= 5', name='cc_rating_rating'),)
+
+
+class Ingredient(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    recipe_id = db.Column(db.Integer, db.ForeignKey('recipe.id'), nullable=False)
+    food_item_id = db.Column(db.Integer, db.ForeignKey('food_item.id'), nullable=False)
+    food_item_equivalent_id = db.Column(db.Integer, db.ForeignKey('food_item_equivalent.id'), nullable=False)
+    quantity = db.Column(db.Float, nullable=False)
+
+    recipe = db.relationship("Recipe", back_populates="ingredients")
+    food_item = db.relationship("FoodItem")
+    food_item_equivalent = db.relationship("FoodItemEquivalent")
+
+    __table_args__ = (CheckConstraint('quantity > 0', name='cc_ingredient_quantity'),)
+
+
+class FoodItem(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_item_category_id = db.Column(db.Integer, db.ForeignKey('food_item_category.id'), nullable=False)
+    name = db.Column(db.String(128), nullable=False)
+    emission_per_kg = db.Column(db.Float, nullable=False)
+    vegan = db.Column(db.Boolean, nullable=False)
+    organic = db.Column(db.Boolean, nullable=False)
+    domestic = db.Column(db.Boolean, nullable=False)
+
+    food_item_category = db.relationship("FoodItemCategory", back_populates="food_items")
+    food_item_equivalents = db.relationship("FoodItemEquivalent", back_populates="food_item")
+
+    __table_args__ = (
+        CheckConstraint('length(name) >= 1', name='cc_food_item_name'),
+        CheckConstraint('emission_per_kg > 0', name='cc_emission_per_kg'),
+    )
+
+
+class FoodItemEquivalent(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    food_item_id = db.Column(db.Integer, db.ForeignKey('food_item.id'), nullable=False)
+    unit_type = db.Column(db.Integer, nullable=False)
+    conversion_factor = db.Column(db.Float, nullable=False)
+
+    food_item = db.relationship("FoodItem", back_populates="food_item_equivalents")
+
+    __table_args__ = (
+        CheckConstraint('unit_type > 0', name='cc_unit_type_enum_defined'),
+        CheckConstraint('conversion_factor >= 0', name='cc_conversion_factor'),
+    )
+
+
+class FoodItemCategory(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(64), nullable=False)
+
+    food_items = db.relationship("FoodItem", back_populates="food_item_category")
+
+    __table_args__ = (
+        CheckConstraint('length(name) >= 1', name='cc_food_item_category_name'),
+    )
--- tests/db_test.py
+++ tests/db_test.py
@@ -10,7 +10,7 @@ from sqlalchemy.exc import IntegrityError
 
 
 from climatecook import create_app, db
-from climatecook.models import Recipe, RecipeCategory
+from climatecook.models import Rating, Recipe, RecipeCategory
 
 @event.listens_for(Engine, "connect")
 def set_sqlite_pragma(dbapi_connection, connection_record):
@@ -39,6 +39,9 @@ def app_handle():
 
 
 def test_recipe_create(app_handle):
+    """
+    Add valid recipe
+    """
     recipe = Recipe(
         name="donkey-recipe"
     )
@@ -46,6 +49,34 @@ def test_recipe_create(app_handle):
         db.session.add(recipe)
         db.session.commit()
         assert Recipe.query.count() == 1
+
+
+def test_recipe_create_with_category(app_handle):
+    """
+    Create recipe with category
+    """
+    recipe_id = random.randint(1, 10000000)
+    recipe_category_id = random.randint(1, 10000000)
+
+    recipe_category = RecipeCategory(
+        id=recipe_category_id,
+        name="donkeyfood"
+    )
+
+    recipe = Recipe(
+        id=recipe_id,
+        name="donkey-recipe",
+        recipe_category_id=recipe_category.id
+    )
+
+    with app_handle.app_context():
+        db.session.add(recipe_category)
+        db.session.add(recipe)
+        db.session.commit()
+        recipe = Recipe.query.filter_by(id=recipe_id).first()
+        recipe_category = RecipeCategory.query.filter_by(id=recipe_category_id).first()
+        assert recipe.recipe_category.id == recipe_category_id
+        assert recipe_category.recipes[0].id == recipe.id
 
 
 def test_recipe_create_with_invalid_category(app_handle):
@@ -65,7 +96,7 @@ def test_recipe_create_with_invalid_category(app_handle):
 
 def test_recipe_create_with_invalid_name(app_handle):
     """
-    Try adding a recipe with a non-existing category
+    Try adding a recipe with empty name
     """
     recipe = Recipe(
         name="",
@@ -74,8 +105,86 @@ def test_recipe_create_with_invalid_name(app_handle):
         db.session.add(recipe)
         with pytest.raises(IntegrityError):
             db.session.commit()
-            
 
-def test_create_recipe_category(app_handle):
 
-    pass
+def test_recipe_category_create(app_handle):
+    """
+    Add valid recipe category
+    """
+    recipe_category = RecipeCategory(
+        name="donkeyfood"
+    )
+    with app_handle.app_context():
+        db.session.add(recipe_category)
+        db.session.commit()
+        assert RecipeCategory.query.count() == 1
+
+
+def test_rating_create(app_handle):
+    """
+    Add valid rating
+    """
+    recipe_id = random.randint(1, 10000000)
+    rating_id = random.randint(1, 10000000)
+    recipe = Recipe(
+        id=recipe_id,
+        name="donkey_recipe"
+    )
+    rating = Rating(
+        id=rating_id,
+        recipe_id=recipe_id,
+        rating=5,
+        comment="this is donkeyfood!"
+    )
+
+    with app_handle.app_context():
+        db.session.add(recipe)
+        db.session.add(rating)
+        db.session.commit()
+        assert Rating.query.count() == 1
+        rating = Rating.query.filter_by(id=rating_id).first()
+        assert rating.recipe.id == recipe_id
+
+
+def test_rating_create_invalid_recipe(app_handle):
+    """
+    Try to add rating with invalid recipe id
+    """
+    recipe_id = random.randint(1, 10000000)
+    rating_id = random.randint(1, 10000000)
+    
+    rating = Rating(
+        id=rating_id,
+        recipe_id=recipe_id,
+        rating=5,
+        comment="this is donkeyfood!"
+    )
+
+    with app_handle.app_context():
+        db.session.add(rating)
+        with pytest.raises(IntegrityError):
+            db.session.commit()
+
+
+def test_rating_create_with_invalid_rating(app_handle):
+    """
+    Try to add rating with invalid rating
+    """
+    recipe_id = random.randint(1, 10000000)
+    rating_id = random.randint(1, 10000000)
+    recipe = Recipe(
+        id=recipe_id,
+        name="donkey_recipe"
+    )
+    rating = Rating(
+        id=rating_id,
+        recipe_id=recipe_id,
+        rating=0,
+        comment="this is donkeyfood!"
+    )
+
+    with app_handle.app_context():
+        db.session.add(recipe)
+        db.session.add(rating)
+        with pytest.raises(IntegrityError):
+            db.session.commit()
